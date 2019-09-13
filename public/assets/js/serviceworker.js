b9d57cf7189d0fb9f0ecc1ae789324e795b4fbb9
Nicolas Santos <nicolas.santos@bompracredito.onmicrosoft.com>
2019-09-13 15:41:44 -0300

    pwa

--- public/assets/js/serviceworker.js
+++ public/assets/js/serviceworker.js
@@ -1,19 +1,21 @@
-self.addEventListener('install', event => {
-  event.waitUntil( 
-    caches.open('bpcCache').then(function(cache) {
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('bpcCache').then(function (cache) {
       return cache.addAll([
         '/assets/images/background-home.jpg',
         '/assets/images/Logo_horizontal.png'
       ]);
     })
   );
-}); 
+});
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
+  console.log('event');
   event.respondWith(
-    caches.open('bpcCache').then(function(cache) {
+    caches.open('bpcCache').then(function (cache) {
+      console.log(cache);
       return cache.match(event.request).then(function (response) {
-        return response || fetch(event.request).then(function(response) {
+        return response || fetch(event.request).then(function (response) {
           cache.put(event.request, response.clone());
           return response;
         });
